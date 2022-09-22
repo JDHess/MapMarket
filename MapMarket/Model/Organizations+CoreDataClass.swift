952ec0c2425c0fe27871ee337ebeef170fb30cc1
Julie Hess <jul>
2022-09-22 06:05:24 -0700

    Updated Core Data Model

---
+++ MapMarket/Model/Organizations+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Organizations+CoreDataClass.swift
+//  MapMarket
+//
+//  Created by Julie Hess on 9/22/22.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Organizations)
+public class Organizations: NSManagedObject {
+
+}
